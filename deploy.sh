7fed81720272a81bde714f04abe88d557ddf42a2
Alp Eren Kose <alperenkose@gmail.com>
2010-07-26 22:42:56 +0000

    Forgot to change deploy.sh for executable name change..
    git-svn-id: svn+ssh://svn.gna.org/svn/harix/trunk@37 93407c37-4ef6-48a3-b47d-bdf02b9e85ca

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-install -m 0755 query_pcimap.wt /srv/http/harixB
+install -m 0755 harix.wt /srv/http/harixB
 rm /srv/http/harixB/queryDistro.sh
 install -m 0755 queryDistro.sh /srv/http/harixB
 cp -R resources /srv/http/harixB
